c252b9472952475ec866a1c5fb3487303bf79124
MarcielRocha <marcielrochapj@gmail.com>
2023-05-28 21:35:02 -0300

    Agora com situação de conflito Marciel

--- Trabalho-1/Exemplo01/Program.cs
+++ Trabalho-1/Exemplo01/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World Marciel!");
 //Alteracao feita por Marciel
+Console.WriteLine(char.ToUpper('c'));             // C
+Console.WriteLine(char.IsWhiteSpace('\t'));       // True
+Console.WriteLine(char.IsLetter('x'));            // True
+Console.WriteLine(char.GetUnicodeCategory('x'));  // LowercaseLetter
