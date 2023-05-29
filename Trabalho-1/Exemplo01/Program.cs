06f0d639351ff6e067caaaa47db23a0e083d0bf5
BiaCalifornia13 <bia.sdca16@gmail.com>
2023-05-28 21:36:19 -0300

    Situação de conflito Bia

--- Trabalho-1/Exemplo01/Program.cs
+++ Trabalho-1/Exemplo01/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Mundo");
+// Alteração realizada por Ana Beatriz
+Console.WriteLine(new string('*', 10));    // **********
+
+char[] ca = "Hello".ToCharArray();
+string s = new string(ca);                 // Reconstrução de string a partir de vetor de chars
+Console.WriteLine(s);                      // "Hello"
+
